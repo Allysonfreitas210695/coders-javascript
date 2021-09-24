9b5960853e36269219ce53395150c70cf5782d9b
Allysonfreitas210695 <allysonfr83@gmail.com>
2021-09-24 15:17:45 -0300

    ajeitando o aviso no alert

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const usuários = [
   {
     email: "allyson@123.com",
-    senha: "123"
+    senha: "123456"
   },
   {
     email: "bruno@123.com",
@@ -13,16 +13,22 @@ function login() {
   let email = document.querySelector("input#email").value;
   let senha = document.querySelector("input#senha").value;
   let acesso = false;
+  let isVazio = false;
+  let result = '';
 
   if(email === '' || senha === ''){
-    alert("Campo "+(email == '' && senha === '' ? 'Email e senha estão vazio!' : (senha == '' ? 'Senha estão vazio' : 'email estão vazio')));
+    isVazio = true;
+    result += (senha === '' && email === ''? 'Email e Senha estão vazio' : (senha === '' ? 'Senha estão vazio' : 'email estão vazio'));
   }
-  
+
   for(let user of usuários) {
      if(user.email === email && user.senha === senha) {
        acesso = true;
      }
   }
 
-  alert((!acesso ? "Negado"  : "Acesso Permitido"))
+  alert((!acesso || !isVazio ? "Negado"  : "Acesso Permitido"))
+  if(isVazio){
+    alert(result);
+  }
 }
